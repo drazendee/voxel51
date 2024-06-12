ac548d650747a5790c6191b3caa228de595d921d
sofiacharnota <sofiacharnota@gmail.com>
2024-06-12 16:10:00 +0200

    add env vars

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import os
+
+# Set environment variables before importing FiftyOne
+os.environ["FIFTYONE_DATABASE_DIR"] = "/tmp/db/"
+os.environ["FIFTYONE_DEFAULT_DATASET_DIR"] = "/tmp/db/"
+os.environ["FIFTYONE_DATASET_ZOO_DIR"] = "/tmp/db_zoo/"
+os.environ["FIFTYONE_MODEL_ZOO_DIR"] = "/tmp/zoo_model/"
+
 import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
 
-fo.config.database_dir = "/tmp/db/"
-fo.config.default_dataset_dir = "/tmp/db/"
-fo.config.dataset_zoo_dir = "/tmp/db_zoo/"
-fo.config.model_zoo_dir = "/tmp/zoo_model/"
+# Ensure FiftyOne reads the configurations from the environment variables
+fo.config.database_dir = os.environ["FIFTYONE_DATABASE_DIR"]
+fo.config.default_dataset_dir = os.environ["FIFTYONE_DEFAULT_DATASET_DIR"]
+fo.config.dataset_zoo_dir = os.environ["FIFTYONE_DATASET_ZOO_DIR"]
+fo.config.model_zoo_dir = os.environ["FIFTYONE_MODEL_ZOO_DIR"]
 
 # Load dataset
 dataset = foz.load_zoo_dataset("quickstart")
-
 
 # Launch the FiftyOne app
 session = fo.launch_app(dataset, address='0.0.0.0', port=5151, remote=True)
